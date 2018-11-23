bec108eb7045c3763b93c33d6d8940840d19f45a
Ayyaz Khan <khan.shan803@gmail.com>
2018-11-24 02:01:59 +0530

    14th commit

--- app/src/androidTest/java/com/newyorktimesapi/example/ResultPresenterTest.java
+++ app/src/androidTest/java/com/newyorktimesapi/example/ResultPresenterTest.java
@@ -1,8 +1,28 @@
 package com.newyorktimesapi.example;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by ayyazkhan on 24/11/18.
  */
 
 public class ResultPresenterTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void presenterTest() {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
 }
