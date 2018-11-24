31a4bfbbb8fec2f0923957f4ff5714ecc6e871a6
Ayyaz Khan <khan.shan803@gmail.com>
2018-11-24 16:44:27 +0530

    final commit

--- app/src/androidTest/java/com/newyorktimesapi/example/ResultPresenterTest.java
+++ app/src/androidTest/java/com/newyorktimesapi/example/ResultPresenterTest.java
@@ -1,5 +1,7 @@
 package com.newyorktimesapi.example;
 
+import android.support.test.rule.ActivityTestRule;
+
 import com.newyorktimesapi.example.Utils.Utility;
 import com.newyorktimesapi.example.View.IResultView;
 import com.newyorktimesapi.example.network.Retrofit.RFInterface;
@@ -14,40 +16,49 @@ import org.junit.Test;
 import retrofit2.Call;
 import retrofit2.Response;
 
+import static junit.framework.Assert.assertNotNull;
+
 /**
  * Created by ayyazkhan on 24/11/18.
  */
 
 public class ResultPresenterTest {
 
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    private MainActivity mainActivity = null;
+
+
     private RFInterface rfInterface = null;
 
     private ResultPresenter resultPresenterv;
-
-    private IResultView iResultView;
 
     private Response<RmData> rmDataResponse = null;
 
 
     @Before
     public void setUp() throws Exception {
+
+        mainActivity = activityTestRule.getActivity();
+
         rfInterface = Utility.getRetrofitInterface(MainActivity.service);
 
         Call<RmData> rmDataCall = rfInterface.getDetails(MainActivity.apikey);
         rmDataResponse = rmDataCall.execute();
 
-        resultPresenterv= new ResultPresenter(iResultView, rmDataResponse.body().getResults());
+        resultPresenterv= new ResultPresenter(mainActivity, rmDataResponse.body().getResults());
 
     }
 
     @Test
     public void presenterTest() {
-
+        //TODO
     }
 
     @After
     public void tearDown() throws Exception {
-
+        mainActivity.finish();
     }
 
 }
